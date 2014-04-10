397c79a637ef82b391bb19e74b54a9b2382386dc
sedouard <steven.edouard1@gmail.com>
2014-04-10 02:04:08 -0700

    delete this version of the app

--- Win8/SmartDoor/services/mobile services/smartdoor/service.js
+++
@@ -1,7 +0,0 @@
-﻿// http://go.microsoft.com/fwlink/?LinkID=290993&clcid=0x409
-var g_smartdoorClient = new WindowsAzure.MobileServiceClient(
-                "https://smartdoor.azure-mobile.net/",
-                "HnAxDFiMSQATHNaCcgIpEllAhenguj14");
-var g_authToken;
-var g_userId;
-var g_applicationData = Windows.Storage.ApplicationData.current;
